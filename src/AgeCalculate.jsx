6741e7e6a07c8fc98fdb8d24d0c302b06e8747f8
SoJuSo <asdfg7123@gmail.com>
2023-11-16 16:18:04 +0900

    [Feature 1][Feat] Add defensive code for the number of days in February

--- src/AgeCalculate.jsx
+++ src/AgeCalculate.jsx
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const AgeCalculate = () => {
   const currentYear = new Date().getFullYear();
-  const years = Array.from({ length: 80 }, (_, index) => currentYear - index);
+  const years = Array.from({ length: 10 }, (_, index) => currentYear - index);
   const months = Array.from({ length: 12 }, (_, index) => index + 1);
-  const days = Array.from({ length: 31 }, (_, index) => index + 1);
 
   const [selectedYear, setSelectedYear] = useState(currentYear);
   const [selectedMonth, setSelectedMonth] = useState(1);
   const [selectedDay, setSelectedDay] = useState(1);
+
+  useEffect(() => {
+    updateDaysInMonth();
+  }, [selectedMonth]);
+
+  const updateDaysInMonth = () => {
+    const daysInMonth = new Date(selectedYear, selectedMonth, 0).getDate();
+    setSelectedDay((prevDay) => (prevDay > daysInMonth ? daysInMonth : prevDay));
+  };
 
   const handleYearChange = (e) => {
     setSelectedYear(parseInt(e.target.value));
@@ -44,11 +52,13 @@ const AgeCalculate = () => {
 
       <label htmlFor="day">Select a day:</label>
       <select id="day" value={selectedDay} onChange={handleDayChange}>
-        {days.map((day) => (
-          <option key={day} value={day}>
-            {day}
-          </option>
-        ))}
+        {Array.from({ length: new Date(selectedYear, selectedMonth, 0).getDate() }, (_, index) => index + 1).map(
+          (day) => (
+            <option key={day} value={day}>
+              {day}
+            </option>
+          )
+        )}
       </select>
 
       <p>
