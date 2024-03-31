8deed376b7c5cda2099e750f83a8dc06c93eecde
SoJuSo <asdfg7123@gmail.com>
2024-03-31 14:48:27 +0900

    💄design : create NotFound page design

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,7 +1,22 @@
 const NotFound = () => {
   return (
     <>
-      <h2>Can't find this page!!!</h2>
+      <main style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <h2>Sorry, we couldn't find that page</h2>
+        <a
+          href="/"
+          style={{
+            color: "#ddd",
+            textDecoration: "none",
+            backgroundColor: "#123456",
+            padding: ".5rem",
+            boxSizing: "border-box",
+            borderRadius: "5px",
+          }}
+        >
+          Back to Home
+        </a>
+      </main>
     </>
   );
 };
