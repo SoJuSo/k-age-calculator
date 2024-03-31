77213156b828b29decf71d053763b323498a50c7
SoJuSo <asdfg7123@gmail.com>
2024-03-31 14:03:37 +0900

    ♻️refactor : Convert the main code into functional

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,6 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);
